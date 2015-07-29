e24d35ebb395ba9074b8ee669f43496a8751227c
Elijah Insua <tmpvar@gmail.com>
2015-07-29 00:57:20 -0400

    tweak tests

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var test = require('tape');
 var solve = require('./solver');
 var constraints = require('./constraints');
 
-var EPS = 1e-10;
+var EPS = 1e-9;
 
 function near(a, b) {
   if  (Math.abs(a-b) < EPS) {
@@ -105,21 +105,23 @@ test('basic pointOnLine test', function(t) {
   ];
 
   var pointOnLine = [constraints.pointOnLine, [point, line]];
-  // var l1p0Fixed = [constraints.fixed, [line[0]]];
-  // var l1p1Fixed = [constraints.fixed, [line[1]]];
+  var res = solve([pointOnLine]);
 
-  var res = solve([pointOnLine]);//, l1p0Fixed, l1p1Fixed]);
   t.ok(res, 'found a solution');
   t.ok(near(point[0], 0.5), 'point1 x near 0.5');
   t.ok(near(point[1], 1/3), 'point1 y near 0.333333');
-  // t.ok(near(point2[0], 0.5), 'point2 x near 0');
-  // t.ok(near(point2[1], 0.5), 'point2 y near 0.5');
+
+  t.ok(near(line[0][0], 0.0), 'line.start x near 0.0');
+  t.ok(near(line[1][0], 1.0), 'line.end x near 1.0');
+
+  t.ok(near(line[0][1], 1/3), 'line.start x near 1/3');
+  t.ok(near(line[1][1], 1/3), 'line.end x near 1/3');
   t.end();
 });
 
 
 
-test('basic pointOnLine test', function(t) {
+test('basic pointOnLine test with fixed points', function(t) {
   var point = [.5, 1];
 
   var line = [
